8ecd1dcc99f0f36351083472b7affa7bba487eb4
sagiv levi <sagivl@cellebrite.com>
2017-01-04 12:46:09 +0200

    da

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,11 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
+  
 /**
 * @param blab
 * @ return blab
-**/
-  
+*/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
